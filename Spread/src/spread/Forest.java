30e0a281f188de5decfbd3cf12fa4481c0443c65
Kritpitharn <anfewww@gmail.com>
2014-11-20 09:01:12 +0700

    final
    probcacth is on process

---
+++ Spread/src/spread/Forest.java
@@ -0,0 +1,233 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spread;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author tAnniiz-PC
+ */
+public class Forest extends JPanel {
+    private int width, height, probC, probT, probB;
+    private Tree[][] tree;
+    
+    public Forest(){
+        this.width = 50;
+        this.height = 50;
+        this.probC = 0;
+        this.probT = 100;
+        this.probB = 0;
+        this.tree = new Tree[this.width][this.width];
+
+        this.setLayout(new GridLayout(this.width, this.height));
+        
+        for(int i = 0 ; i < this.height ; i++){
+            for(int j = 0 ; j < this.width ; j++){
+                if(i == 0 || i == this.width - 1 || j == 0 || j == this.height - 1){
+                    tree[i][j] = new Tree(0);
+                    this.add(tree[i][j]);
+                } else {
+                    if(this.random(this.probT)){
+                        if(this.random(this.probB)) {
+                            tree[i][j] = new Tree(2);
+                            this.add(tree[i][j]);
+                        } else {
+                            tree[i][j] = new Tree(1);
+                            this.add(tree[i][j]);
+                        }
+                    } else {
+                        tree[i][j] = new Tree(0);
+                        this.add(tree[i][j]);
+                    }
+                }
+            }
+        }
+        
+        tree[this.width / 2][this.height / 2].setState(2);
+        update();
+    }
+    
+    public Forest(int width, int height, int probB, int probC, int probT){
+        this.width = width;
+        this.height = height;
+        this.probC = probC;
+        this.probT = probT;
+        this.probB = probB;
+        this.setLayout(new GridLayout(this.width, this.height));
+        
+        this.tree = new Tree[this.width][this.height];
+        
+        for(int i = 0 ; i < this.height ; i++){
+            for(int j = 0 ; j < this.width ; j++){
+                if(i == 0 || i == this.width - 1 || j == 0 || j == this.height - 1){
+                    tree[i][j] = new Tree(0);
+                    this.add(tree[i][j]);
+                } else {
+                    tree[i][j] = new Tree(1);
+                    this.add(tree[i][j]);
+                }
+            }
+        }
+        
+        tree[this.width / 2][this.height / 2].setState(2);
+        update();
+    }
+    
+    public boolean random(int prob){
+        return (int)(Math.random()*100) < prob;
+    }
+    
+    public void spread(){
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+        
+        for (int i = 0; i < this.height - 1; i++) {
+            for (int j = 0; j < this.width - 1; j++) {
+                if (tree[i][j].getState() == Tree.RED && tree[i][j].isFire()) {
+                    tree[i][j].setState(Tree.YELLOW);
+                    burn('N', i, j);
+                    burn('S', i, j);
+                    burn('W', i, j);
+                    burn('E', i, j);
+                    this.update(); 
+                }
+            }
+        }
+        
+        for (int i = 0; i < this.height - 1; i++) {
+            for (int j = 0; j < this.width - 1; j++) {
+                if (tree[i][j].getState() == Tree.RED) {
+                    tree[i][j].setFire(true);
+                }
+            }
+        }
+    }
+    
+    public void burn(char dir, int posX, int posY) {
+        switch(dir){
+            case 'N': if(tree[posX][posY - 1].getState() == 1) tree[posX][posY - 1].setState(2); tree[posX][posY - 1].setFire(false); break;
+            case 'E': if(tree[posX + 1][posY].getState() == 1) tree[posX + 1][posY].setState(2); tree[posX + 1][posY].setFire(false); break;
+            case 'W': if(tree[posX - 1][posY].getState() == 1) tree[posX - 1][posY].setState(2); tree[posX - 1][posY].setFire(false); break;
+            case 'S': if(tree[posX][posY + 1].getState() == 1) tree[posX][posY + 1].setState(2); tree[posX][posY + 1].setFire(false); break;
+        }
+    }
+    
+    public boolean isEnd(){
+        for (int i = 0; i < this.height - 1; i++) {
+            for (int j = 0; j < this.width - 1; j++) {
+                if (tree[i][j].getState() == Tree.RED) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+    
+    public void setSize(int width, int height){
+        this.width = width;
+        this.height = height;
+        this.setLayout(new GridLayout(this.width, this.height));
+        this.tree = new Tree[this.width][this.height];
+        this.reset();
+    }
+    
+    public void reset(){
+        this.removeAll();
+        for(int i = 0 ; i < this.height ; i++){
+            for(int j = 0 ; j < this.width ; j++){
+                if(i == 0 || i == this.width - 1 || j == 0 || j == this.height - 1){
+                    tree[i][j] = new Tree(0);
+                    this.add(tree[i][j]);
+                } else {
+                    if(this.random(this.probT)){
+                        if(this.random(this.probB)) {
+                            tree[i][j] = new Tree(2);
+                            this.add(tree[i][j]);
+                        } else {
+                            tree[i][j] = new Tree(1);
+                            this.add(tree[i][j]);
+                        }
+                    } else {
+                        tree[i][j] = new Tree(0);
+                        this.add(tree[i][j]);
+                    }
+                }
+            }
+        }
+        
+        tree[this.width / 2][this.height / 2].setState(2);
+        update();
+    }
+
+    public void setProbC(int probC) {
+        this.probC = probC;
+    }
+
+    public void setProbT(int probT) {
+        this.probT = probT;
+    }
+
+    public void setProbB(int probB) {
+        this.probB = probB;
+    }
+
+    public void update() {
+        this.validate();
+        this.repaint();
+    }
+
+
+    private class Tree extends JLabel {
+        private final static int YELLOW = 0, GREED = 1, RED = 2;
+        private int state;
+        private boolean fire;
+        
+        public Tree(int state){
+            this.state = state;
+            this.fire = true;
+            this.setState(this.state);
+            this.setOpaque(true);
+        }
+
+        public boolean isFire() {
+            return this.fire;
+        }
+
+        public void setFire(boolean fire) {
+            this.fire = fire;
+        }
+
+        public int getState() {
+            return state;
+        }
+
+        public void setState(int state) {
+            this.state = state;
+            switch(this.state){
+                case 0:
+                    this.setBackground(Color.YELLOW); this.setText("" + this.state); break;
+                case 1:
+                    this.setBackground(Color.GREEN); this.setText("" + this.state); break;
+                case 2:
+                    this.setBackground(Color.RED); this.setText("" + this.state); break;
+            }
+            Forest.this.update();
+        }
+        
+        
+    }
+}
+
+    
+
---
+++ Spread/src/spread/Main.java
@@ -0,0 +1,154 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spread;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author tAnniiz-PC
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    static Thread auto;
+    static Forest forest;
+    static int counter = 0, width = 10, height = 10, probB = 0, probC = 100, probT = 100;
+    public static void main(String[] args) {
+        
+
+        forest = new Forest(width, height, probB, probC, probT);
+        JFrame frame = new JFrame("SpreadFire");
+        frame.setSize(800,600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        JPanel panel2 = new JPanel();
+        panel2.setLayout(new FlowLayout());
+        panel2.setBorder(BorderFactory.createLineBorder(Color.black));
+        JButton man = new JButton("Manual");
+        panel2.add(man);
+        JButton pause = new JButton("Pause");
+        panel2.add(pause);
+        JButton autospread = new JButton("Auto spraed");
+        panel2.add(autospread);
+        JButton size = new JButton("Set Size");
+        panel2.add(size);
+        JButton reset = new JButton("Reset");
+        panel2.add(reset);
+        JButton probt = new JButton("Probtree");
+        panel2.add(probt);
+        JButton probb = new JButton("Probburn");
+        panel2.add(probb);
+        JButton probc = new JButton("Probcacth");
+        panel2.add(probc);
+        panel2.setBackground(Color.green);
+        frame.add(panel2);
+        frame.add(panel2, BorderLayout.NORTH);
+        
+        frame.add(forest, BorderLayout.CENTER);
+        frame.add(panel2, BorderLayout.NORTH);
+        
+        frame.setVisible(true);
+
+        autospread.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)
+            {
+                auto = new Thread(){
+                    public void run(){
+                        while(!forest.isEnd()){
+                            forest.spread();
+                            counter++;
+                            System.out.println(counter);
+                        }
+                    }
+                };
+                auto.start();
+            }
+        }); 
+        
+        reset.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)
+            {
+                forest.reset();
+            }
+        }); 
+        
+        size.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)
+            {
+                JTextField widthTF = new JTextField();
+                JTextField heightTF = new JTextField();
+                final JComponent[] inputs = new JComponent[] {
+                        new JLabel("Width:"),
+                        widthTF,
+                        new JLabel("Height:"),
+                        heightTF,
+                };
+                JOptionPane.showMessageDialog(null, inputs, "Size Setting", JOptionPane.PLAIN_MESSAGE);
+                
+                width = Integer.parseInt(widthTF.getText());
+                height = Integer.parseInt(heightTF.getText());
+                forest.setSize(width, height);
+            }
+        }); 
+        
+        probb.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)
+            {
+                probB = Integer.parseInt(JOptionPane.showInputDialog("Input ProbBurn: "));
+                forest.setProbB(probB);
+            }
+        }); 
+        
+        probc.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)
+            {
+                probC = Integer.parseInt(JOptionPane.showInputDialog("Input Probcatch: "));
+                forest.setProbB(probC);
+            }
+        }); 
+        
+        probt.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)
+            {
+                probT = Integer.parseInt(JOptionPane.showInputDialog("Input Probtree: "));
+                forest.setProbB(probT);
+            }
+        }); 
+        
+        pause.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)
+            {
+                auto.stop();
+            }
+        }); 
+        
+        man.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)
+            {
+                if(!forest.isEnd()){
+                    forest.spread();
+                }
+            }
+        }); 
+    }  
+}
